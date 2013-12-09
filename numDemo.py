62f6cae9ef5e9852c6451332cda47313455f3e46
Dan <Ryouth@gmail.com>
2013-12-08 21:20:23 -0700

    Added random number

--- numDemo.py
+++ numDemo.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+import random
+
 name = raw_input('Hello! What is your name?\n')
+
+number = random.randint(1, 20)
 
 print 'Hello, {0}'.format(name)
 
+
+print 'The number is {0}'.format(number)
